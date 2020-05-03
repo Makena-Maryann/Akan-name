f01dbd57fd760781d58fd7211e6bd2f4bae2b4ca
Makena-Maryann <maryann.makena00@gmail.com>
2020-05-03 22:59:53 +0300

    Added JavaScript code that prints Akan name in the console

--- js/scripts.js
+++ js/scripts.js
@@ -21,5 +21,73 @@ var day = parseInt(prompt("Enter day:"));
 var result = formula(century, year, month, day);
 
 var dayOfTheWeek = ~~result;
+var female = false;
+var femaleAkanNames = [
+  "Ama",
+  "Akosua",
+  "Adwoa",
+  "Abenaa",
+  "Akua",
+  "Yaa",
+  " Afua",
+];
+var maleAkanNames = [
+  "Kwame",
+  "Kwasi",
+  "Kwadwo",
+  "Kwabena",
+  "Kwaku",
+  "Yaw",
+  "Kofi",
+];
+var akanName;
 
-alert(dayOfTheWeek);
+if (female === true) {
+  switch (dayOfTheWeek) {
+    case 0:
+      console.log(femaleAkanNames[0]);
+      break;
+    case 1:
+      console.log(femaleAkanNames[1]);
+      break;
+    case 2:
+      console.log(femaleAkanNames[2]);
+      break;
+    case 3:
+      console.log(femaleAkanNames[3]);
+      break;
+    case 4:
+      console.log(femaleAkanNames[4]);
+      break;
+    case 5:
+      console.log(femaleAkanNames[5]);
+      break;
+    case 6:
+      console.log(femaleAkanNames[6]);
+      break;
+  }
+} else {
+  switch (dayOfTheWeek) {
+    case 0:
+      console.log(maleAkanNames[0]);
+      break;
+    case 1:
+      console.log(maleAkanNames[1]);
+      break;
+    case 2:
+      console.log(maleAkanNames[2]);
+      break;
+    case 3:
+      console.log(maleAkanNames[3]);
+      break;
+    case 4:
+      console.log(maleAkanNames[4]);
+      break;
+    case 5:
+      console.log(maleAkanNames[5]);
+      break;
+    case 6:
+      console.log(maleAkanNames[6]);
+      break;
+  }
+}
